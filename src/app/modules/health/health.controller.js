9c1b4535204c31252c2368ea4b7c8968cda36819
NeoSonoda <nickar34@gmail.com>
2022-11-13 16:01:27 -0300

    test imports

--- src/app/modules/health/health.controller.js
+++ src/app/modules/health/health.controller.js
@@ -1,3 +1,10 @@
+const repositories = require('./usuarios.repository')
+const { errors } = require('../../services/error')
+const config = require('../../../../config/environment.config')
+
+
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 //Controller: recebe os dados da rota e redireciona pra camada de negócios (business)
 class healthCheck {
@@ -5,6 +12,22 @@ class healthCheck {
     static async healthCheck(req, res) {
 
         console.log("health check init.")
+
+        try {
+
+            let dadosCriados = {"funcional":"true"}
+
+            return res.status(200).json(dadosCriados)
+
+        } catch (error) {
+
+            return res.status(error.code).send(error.message)
+        }
+    }
+
+    static async healthTestCheck(req, res) {
+
+        console.log("health test check init.")
 
         try {
 
